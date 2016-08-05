bdcef7d6955543b3dbb188f007d2735280f234a6
Rafael Sales <rafaelcds@gmail.com>
2016-08-05 11:42:00 -0300

    Fix sortability

--- lib/ulid/generator.rb
+++ lib/ulid/generator.rb
@@ -2,21 +2,23 @@ require 'sysrandom'
 
 module ULID
   class Generator
-    ENCODING = '0123456789ABCDEFGHJKMNPQRSTVWXYZ'
+    ENCODING = '0123456789ABCDEFGHJKMNPQRSTVWXYZ' # Crockford's Base32
+    TIME_LENGTH = 10
+    RANDOM_LENGTH = 16
 
     def generate
-      encode_time(milliseconds_since_epoch, 10) + encode_random(16)
+      encode_time(milliseconds_since_epoch, TIME_LENGTH) + encode_random(RANDOM_LENGTH)
     end
 
     private
 
     def encode_time(now, length)
-      length.times.reduce("") do |output|
+      length.times.reduce('') do |output|
         mod = now % ENCODING.length
         now = (now - mod) / ENCODING.length
 
         output << ENCODING[mod]
-      end
+      end.reverse
     end
 
     def encode_random(length)
@@ -24,7 +26,7 @@ module ULID
         random = Sysrandom.random_number(ENCODING.length)
 
         output << ENCODING[random]
-      end
+      end.reverse
     end
 
     def milliseconds_since_epoch
--- spec/lib/ulid_spec.rb
+++ spec/lib/ulid_spec.rb
@@ -1,9 +1,17 @@
 require 'spec_helper'
 
 describe ULID do
-  it "works" do
+  it "ensures it has 26 chars" do
     ulid = ULID.generate
+
     ulid.length.must_equal 26
+  end
+
+  it "is sortable" do
+    ulid_1 = ULID.generate
+    ulid_2 = ULID.generate
+
+    assert ulid_2 > ulid_1
   end
 end
 
